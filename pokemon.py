c9bad983d06bba29c0075bc12464e86ca1060945
Alvarado <moisoar@amazon.com>
2026-03-08 16:39:18 -0600

    Adding a dictionary attribute to our class for the poke stats

--- pokemon.py
+++ pokemon.py
@@ -3,11 +3,10 @@
 
 
 class Pokemon:
-    def __init__(self, name:str, poke_type:str, level:int):
+    def __init__(self, name:str, poke_type:str, poke_stats:dict):
         self.name: str = name
         self.poke_type: str = poke_type
-        self.level: int = level
-        self.hp: int = level * 20
+        self.poke_stats: dict = poke_stats
 
     def level_up(self):
         self.level += 1
@@ -32,31 +31,42 @@ class Pokedex:
 
     def show_all(self):
         for poke in self.self_entries:
-            print(f"Name: {poke.name} | Level: {poke.level}")
+            print(f"Name: {poke.name} | Type: {poke.poke_type}")
 
     def save_to_file(self, filename: str):
         # 'w' means Write mode (it creates the file or overwrites it)
         with open(filename, 'w') as file:
             for poke in self.self_entries:
                 # We create a string for each pokemon
-                line = f"{poke.name}, {poke.level}, {poke.poke_type}\n\n"
+                line = f"{poke.name}, {poke.poke_type}\n\n"
                 file.write(line)
         print(f"Pokedex saved successfully to {filename}!")
 
-    def load_from_file(self, filename: str):
+''' def load_from_file(self, filename: str):
         try:
             with open(filename, 'r') as file:
                 for line in file:
-                    data = line.strip().split(',')
-                    new_poke = Pokemon(data[0], data[2], int(data[1]))
+                    # Clean the line
+                    clean_line = line.strip()
+
+                    if not clean_line:
+                        continue
+                    data = clean_line.split(',')
+                    # Check if we have all 3 pieces of data (Name, Level, Type)
+                    if len(data) < 3:
+                        print(f"Skipping messy line: {clean_line}")
+                        continue # Moves to the next line instead of crashing
+
+                    new_poke = Pokemon(data[0], data[1], data[])
                     self.add_pokemon(new_poke)
                 print("Data loaded back into the Dex!")
         except FileNotFoundError:
-            print("No save file found. Starting fresh!")
+            print("No save file found. Starting fresh!")'''
     
 
-p1 = Pokemon("Torchic", "Fire", 5)
-p2 = Pokemon("Squirtel", "Water", 5)
+p1 = Pokemon("Torchic", "Fire",
+             {"HP":45, "Attack":49, "Defense":49, "Sp.Atk":65, "Sp.Atk":65, "Sp.Def":65, "Speed":45})
+
 
 '''for level in range(11):
     p1.level_up()
@@ -68,7 +78,8 @@ vael_pokedex.add_pokemon(p1)
 vael_pokedex.add_pokemon(p2)
 print(len(vael_pokedex.self_entries))
 
-vael_pokedex.load_from_file("poke_update.txt")
+#vael_pokedex.load_from_file("poke_update.txt")
 
 vael_pokedex.show_all()
+
 
